67e9bf2e08992665ff103e1f7da19be6cdaf1d30
zzz <59070006@it.kmitl.ac.th>
2018-10-10 23:32:32 +0700

    no message

--- kingdom.py
+++
@@ -1,8 +0,0 @@
-def main():
-    twostar = int(input())
-    threestar = twostar//2
-    fourstar = threestar//3
-    fivestar = fourstar//4
-    sixstar = fivestar//5
-    print("sixstar = " , sixstar)
-main();
